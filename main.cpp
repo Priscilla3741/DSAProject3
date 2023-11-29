11a6f2732bf48dca3495ed66d65c47b95eb0c81e
Priscilla3741 <priscillapoon03@gmail.com>
2023-11-28 21:21:55 -0500

    :<

--- main.cpp
+++ main.cpp
@@ -62,9 +62,38 @@ int main() {
         }
 
         float desiredSector;
+        cout << "Sectors: " << endl;
+        cout << " - Consumer Discretionary" << endl;
+        cout << " - Consumer Staples" << endl;
+        cout << " - Energy" << endl;
+        cout << " - Financials" << endl;
+        cout << " - Health Care" << endl;
+        cout << " - Industrials" << endl;
+        cout << " - Information Technology" << endl;
+        cout << " - Materials" << endl;
+        cout << " - Real Estate" << endl;
+        cout << " - Telecommunication Services" << endl;
+        cout << " - Utilities" << endl;
         cout << "Enter the Sector you want to invest in: ";
 
         cin >> desiredSector;
+
+        // get the stocks in the sector
+
+        int choice;
+        cout << "Enter the number for the category you want to search: ";
+        cout << "1. Price" << endl;
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                int maxPrice;
+                cout << "What is the maximum amount you want to invest in?" << endl;
+                cin >> maxPrice;
+                // with the max price find all the stocks that match are fall under the max price
+        }
+
+
 
     }
 

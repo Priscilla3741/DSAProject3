7d90cb68ea85929ff4942a557e4a49445d666a29
Priscilla3741 <priscillapoon03@gmail.com>
2023-11-28 20:34:42 -0500

    :(

---
+++ Stocks.h
@@ -0,0 +1,20 @@
+//
+// Created by sumye on 11/27/2023.
+//
+
+#ifndef DSAPROJECT3_STOCKS_H
+#define DSAPROJECT3_STOCKS_H
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Stocks {
+
+
+};
+
+
+#endif //DSAPROJECT3_STOCKS_H
--- main.cpp
+++ main.cpp
@@ -3,9 +3,29 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include "Stocks.h"
 using namespace std;
 
-// find the average for every year and then use that to use the regression point (number) for each stock and then use that to see if its good or not (neg or pos)
+struct Stock {
+    string symbol;
+    string name;
+    string sector;
+    float price;
+    float pricePerEarnings;
+    float dividendYield;
+    float earningsPerShare;
+    float weekLow52;
+    float weekHigh52;
+    float marketCap;
+    float ebitda;
+    float pricePerSale;
+    float pricePerBook;
+
+    Stock(const string& sym, const string& n, const string& sec, float p, float pe, float dy, float es, float wl, float wh, float mc, float eb, float ps, float pb)
+            : symbol(sym), name(n), sector(sec), price(p), pricePerEarnings(pe), dividendYield(dy), earningsPerShare(es),
+              weekLow52(wl), weekHigh52(wh), marketCap(mc), ebitda(eb), pricePerSale(ps), pricePerBook(pb) {}
+};
 
 int main() {
     ifstream stockInfo("Stocks.csv");
@@ -14,9 +34,12 @@ int main() {
         cout << "Error opening file Stocks.csv" << endl;
     }
 
+    unordered_map<string, vector<Stock>> stockMap;
+
     if (stockInfo.is_open()) {
         string fileData;
         getline(stockInfo, fileData);
+
         while (getline(stockInfo, fileData)) {
             string symbol, name, sector, sec;
             string p,pe,dy,es,wl,wh,mc,eb,ps,pb;
@@ -37,8 +60,6 @@ int main() {
             getline(stream, ps,',');
             getline(stream, pb,',');
 
-            // fix float - throws problem when the number is negative and has to change to a float
-
             float price = stof(p);
             float pricePerEarnings = stof(pe);
             float dividendYield = stof(dy);
@@ -52,10 +73,14 @@ int main() {
 
             getline(stream, sec, ',');
 
-            cout << "Symbol: " << symbol << ", Name: " << name << ", Sector: " << sector << ", Price: " << price << ", Price/Earnings: " << pricePerEarnings << ", Dividend Yield: " << dividendYield
-                << ", Earnings/Share: " << earningsPerShare << ", 52 Week Low: " << weekLow52 << ", 52 Week High: " << weekHigh52
-                << ", Market Cap: " << marketCap << ", EBITDA: " << ebitda << ", Price/Sale: " << pricePerSale << ", Price/Book: " << pricePerBook << endl;
-            cout << endl;
+            Stock stock(symbol, name, sector, price, pricePerEarnings, dividendYield, earningsPerShare, weekLow52, weekHigh52, marketCap, ebitda, pricePerSale, pricePerBook);
+
+            stockMap[sector].push_back(stock);
+        }
+
+        for (const auto& inputs : stockMap) {
+            const string& secName = inputs.first;
+            const vector<Stock>& sectorStocks = inputs.second;
         }
     }
 
